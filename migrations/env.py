f71bd540818f30b591080ae4ecf30f80a5c393ab
DeniO <pufik_22sm@mail.ru>
2025-03-03 20:17:02 +0700

    migrations fix

--- migrations/env.py
+++ migrations/env.py
@@ -59,7 +59,7 @@ def run_migrations_offline() -> None:
 
 
 def do_run_migrations(connection: Connection) -> None:
-    context.configure(connection=connection, target_metadata=target_metadata)
+    context.configure(connection=connection, target_metadata=target_metadata, compare_server_default=True)
 
     with context.begin_transaction():
         context.run_migrations()
