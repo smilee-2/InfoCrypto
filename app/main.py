ae90a99549b2559d3b0ae57176d79fc057186b4b
DeniO <pufik_22sm@mail.ru>
2025-03-04 20:01:06 +0700

    delete fronted, create second branch

--- app/main.py
+++ app/main.py
@@ -29,33 +29,12 @@ app.include_router(router_users)
 
 BASE_DIR = Path(__file__).parent.parent
 
-
-templates_auth = Jinja2Templates(directory=f'{BASE_DIR}/fronted/auth_page')
 templates_main = Jinja2Templates(directory=f'{BASE_DIR}/fronted/main_page')
-templates_user = Jinja2Templates(directory=f'{BASE_DIR}/fronted/user_page')
-templates_admin = Jinja2Templates(directory=f'{BASE_DIR}/fronted/admin_page')
-
-
-@app.get('/', tags=['Main'])
-async def main(request: Request):
-    return templates_auth.TemplateResponse('index.html', {'request': request})
-
 
 @app.get('/main_page', tags=['Main'])
 async def main_page(request: Request, user: Annotated[UserModel, Depends(depends.get_current_user)]):
     return templates_main.TemplateResponse('index.html', {'request': request})
 
-
-@app.get('/user_page', tags=['Main'])
-async def user_page(request: Request, user: Annotated[UserModel, Depends(depends.get_current_user)]):
-    return templates_user.TemplateResponse('index.html', {'request': request})
-
-
-@app.get('/admin_page', tags=['Main'])
-async def admin_page(request: Request, admin: Annotated[UserRootModel, Depends(depends.get_current_user)]):
-    if admin.root == 'admin':
-        return templates_admin.TemplateResponse('index.html', {'request': request})
-    raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='not admin')
 
 if __name__ == '__main__':
     uvicorn.run('main:app', host='0.0.0.0')
